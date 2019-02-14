3b05aed7e3c28a4b218663a5d58bb7e065962500
前端开发部 - 陈骥 <chenji@jiguang.cn>
2019-02-14 16:04:04 +0800

    npm scripts

--- 起步/webpack-demo/webpack.config.js
+++ 起步/webpack-demo/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'main1.js',
         path: path.resolve(__dirname, 'dist')
     }
 }
