e8d2512efdcbc1135e26aa7d7617ae852639417f
前端开发部 - 陈骥 <chenji@jiguang.cn>
2019-02-14 15:21:52 +0800

    添加bundle文件

--- 起步/webpack-demo/src/index.js
+++ 起步/webpack-demo/src/index.js
@@ -1,7 +1,10 @@
+import _ from 'lodash'
+
 function component() {
     var element = document.createElement('div')
 
     // Lodash(目前是通过一个 script 脚步引入)对于执行这一行是必须的
+    // Lodash 现在通过import引入进来
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
     return element
