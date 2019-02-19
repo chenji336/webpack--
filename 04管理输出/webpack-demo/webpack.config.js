f3f9f042bd6976746380ab8a798c164ac847d5a7
前端开发部 - 陈骥 <chenji@jiguang.cn>
2019-02-19 23:46:23 +0800

    回退处理

--- 04管理输出/webpack-demo/webpack.config.js
+++ 04管理输出/webpack-demo/webpack.config.js
@@ -7,43 +7,4 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            /*
-                            * 会发现img是相对于output里面的路径
-                            * 输出：dist/img/icon-17b1b9.png
-                            */
-                            name: 'img/[name]-[hash:6].[ext]'
-                        }
-                    }
-
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
 }
