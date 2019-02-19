cf7db6f2a45e4e43ff42fd477640832dc025a41f
前端开发部 - 陈骥 <chenji@jiguang.cn>
2019-02-19 23:43:03 +0800

    --管理输出--

---
+++ 04管理输出/webpack-demo/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            /*
+                            * 会发现img是相对于output里面的路径
+                            * 输出：dist/img/icon-17b1b9.png
+                            */
+                            name: 'img/[name]-[hash:6].[ext]'
+                        }
+                    }
+
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    }
+}
