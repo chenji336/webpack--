0c92748e86c3c348d579d2e7c4617dc036641834
前端开发部 - 陈骥 <chenji@jiguang.cn>
2019-02-23 15:31:10 +0800

    --开发--

---
+++ 05开发/webpack-demo/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [ // 放在entry后面和output前面，应该是说plugin是对entry进行操作的，顺序是在output之前
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist') // 要求必须是absolute path
+    },
+}
