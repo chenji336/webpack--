257439b9cc7225414b2029b152c2ffca0373d769
前端开发部 - 陈骥 <chenji@jiguang.cn>
2019-09-30 08:27:15 +0800

    fix:重新认识 webpack.DefinePlugiin

--- 08生产环境构建/webpack-demo/src/index.js
+++ 08生产环境构建/webpack-demo/src/index.js
@@ -1,6 +1,7 @@
 import { cube, square } from './math'
 import './style.css'
 
+console.log('process.env.test:', process.env.test)
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!')
 }
--- 08生产环境构建/webpack-demo/webpack.dev.js
+++ 08生产环境构建/webpack-demo/webpack.dev.js
@@ -1,10 +1,26 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
-
+const webpack = require('webpack')
+console.log('process.env.test:', process.env.test)
 module.exports = merge(common, {
-    mode: 'development',
+    mode: 'development', // 默认会设置 new webpack.DefinePlugin({'process.env.NODE_ENV': '"development"'})
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.test': JSON.stringify('test') // 可以注释掉查看 index.js 中是否可以获取到该值
+        }),
+    ]
 })
